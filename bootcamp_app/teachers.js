2f7b1ee6c83f04cf6f555eebc259ba52df479506
Chansoo Han <devhan1207@gmail.com>
2023-05-03 20:29:46 -0400

    conneted db and js

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,7 +2,6 @@ const { Pool } = require('pg');
 const process = require('process');
 
 const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
 
 const pool = new Pool({
   user: 'labber',
@@ -12,15 +11,18 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
+SELECT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${cohortName}%'
-LIMIT ${limit};
+WHERE cohorts.name LIKE '${cohortName}'
+GROUP BY teacher, cohort
+ORDER BY teacher;
 `)
   .then(res => {
     res.rows.forEach(user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+      console.log(`${user.cohort}: ${user.teacher}`);
     })
   })
   .catch(err => console.error('query error', err.stack));
